7b04b722c863d0edf4f07a10a8eeb6331a19a1bd
Fernando Rosa <nando.megaman@gmail.com>
2016-03-10 02:12:18 -0300

    ajustado locale/language para pt-BR

--- config/web.php
+++ config/web.php
@@ -6,6 +6,8 @@ $config = [
     'id' => 'cezao',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'pt-BR',
+    'sourceLanguage' => 'pt-BR',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
